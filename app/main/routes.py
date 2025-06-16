e243c521dc55dc3178ae336f1d3734be85abafc4
Sayo <sayomilembo@gmail.com>
2025-06-16 19:01:29 +0300

    fix youtube iframe display

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,24 @@
 from flask import render_template
 from . import main
+import smtplib
 
 
-@main.route("/")
+@main.route("/", methods= ["GET", "POST"])
 def home():
+    """Sending the articles via email"""
+    my_email = "sayomilembo@gmail.com"
+    my_password = "your_password"
+    message_body = "email message"
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email,
+                         password=my_password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="rzedits@yahoo.com",
+            msg=f"Subject: Here are top news from Unherd \n\n {
+            message_body}"
+        )
     return render_template("index.html")
 
