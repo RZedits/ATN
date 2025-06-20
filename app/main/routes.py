c22948fa6da696a7f0506e43b8e5db4af3b4a4a7
Sayo <sayomilembo@gmail.com>
2025-06-20 16:34:35 +0300

    Bulls bible school and sermons and podcasts UI

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,20 +5,31 @@ import smtplib
 
 @main.route("/", methods= ["GET", "POST"])
 def home():
-    """Sending the articles via email"""
-    my_email = "sayomilembo@gmail.com"
-    my_password = "your_password"
-    message_body = "email message"
-
-    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-        connection.starttls()
-        connection.login(user=my_email,
-                         password=my_password)
-        connection.sendmail(
-            from_addr=my_email,
-            to_addrs="rzedits@yahoo.com",
-            msg=f"Subject: Here are top news from Unherd \n\n {
-            message_body}"
-        )
+    # """Sending the articles via email"""
+    # my_email = "sayomilembo@gmail.com"
+    # my_password = "your_password"
+    # message_body = "email message"
+    #
+    # with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    #     connection.starttls()
+    #     connection.login(user=my_email,
+    #                      password=my_password)
+    #     connection.sendmail(
+    #         from_addr=my_email,
+    #         to_addrs="rzedits@yahoo.com",
+    #         msg=f"Subject: Here are top news from Unherd \n\n {
+    #         message_body}"
+    #     )
     return render_template("index.html")
 
+@main.route("/bulls")
+def bulls():
+    return render_template("bulls.html")
+
+@main.route("/podcasts")
+def podcasts():
+    return render_template("podcast.html")
+
+@main.route("/base")
+def base():
+    return render_template("podcast.html")
